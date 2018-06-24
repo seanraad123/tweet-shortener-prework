f9167e7cd06cf632df31845693d9089a15db2a2e
Sean Conrad <seanconrad123@gmail.com>
2018-06-24 18:30:25 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def dictionary(word)
+  new_list = []
+  substitutions = {"hello" => "hi",
+                  "to" => "2",
+                  "two" => "2",
+                  "too" => "2",
+                  "for" => "four",
+                  "be" => "b",
+                  "you" => "u",
+                  "at" => "@",
+                  "and" => "&"}
+  sub_keys = substitutions.keys
+
+
+
+  sub_keys.each do |key|
+    word.map! { |x| x == key ? substitutions[key] : x }.flatten!
+  end
+  print word.join(" ")
+end
+
+def word_substituter(string)
+  a = string.split.each_slice(1).map{|a|a.join ' '}
+  dictionary(a)
+
+end
+
+
+string = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"
+
+
+word_substituter(string)
